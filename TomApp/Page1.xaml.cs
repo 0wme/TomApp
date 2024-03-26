91b0e8282fc1fc112f30802a66d894c3c41ed5f2
Tom Vieira <skixoxgaming@gmail.com>
2024-03-26 14:47:23 +0100

    Modification de Page1 xaml et xaml.cs pour ajouter le carrousel

--- TomApp/Page1.xaml.cs
+++ TomApp/Page1.xaml.cs
@@ -2,10 +2,26 @@
 
 public partial class Page1 : ContentPage
 {
+    List<string> images = new List<string>
+    {
+        "imgcarrousel1.jpeg",
+        "imgcarrousel2.jpeg",
+        "imgcarrousel3.jpeg",
+    };
+
     public Page1()
     {
         InitializeComponent();
+        CarouselImages.ItemsSource = images;
+
+        Device.StartTimer(TimeSpan.FromSeconds(2), () =>
+        {
+            var next = (CarouselImages.Position + 1) % images.Count;
+            CarouselImages.ScrollTo(next);
+            return true;
+        });
     }
+
 
     async void OnButtonClicked(object sender, EventArgs e)
     {
