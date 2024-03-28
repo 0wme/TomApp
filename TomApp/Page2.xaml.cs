25123cea261cfc1cd1e128581a0e3d1188b6a50a
Tom Vieira <skixoxgaming@gmail.com>
2024-03-28 20:22:28 +0100

    Modification de Page2.xaml et Page2.xaml.cs pour mettre en place l'affichage de l'API

--- TomApp/Page2.xaml.cs
+++ TomApp/Page2.xaml.cs
@@ -1,9 +1,22 @@
-﻿namespace TomApp;
+﻿using Microsoft.Maui.Controls;
 
-public partial class Page2 : ContentPage
+namespace TomApp
 {
-	public Page2()
-	{
-		InitializeComponent();
-	}
+    public partial class Page2 : ContentPage
+    {
+        public Page2()
+        {
+            InitializeComponent();
+            BindingContext = new Page2ViewModel();
+        }
+
+        private async void OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var country = (Country)e.CurrentSelection.FirstOrDefault();
+            if (country != null)
+            {
+                await Navigation.PushAsync(new CountryDetailsPage(country));
+            }
+        }
+    }
 }
