004501aabd0d86b035e104ceb552942b50081272
Tom <skixoxgaming@gmail.com>
2024-04-01 00:32:30 +0200

    Création d'une page pour rentrer un nom avant le jeu

---
+++ TomApp/EnterNamePage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TomApp;
+
+public partial class EnterNamePage : ContentPage
+{
+    private GameMode _gameMode;
+
+    public EnterNamePage(GameMode gameMode)
+    {
+        InitializeComponent();
+        _gameMode = gameMode;
+    }
+
+    private async void OnStartGameButtonClicked(object sender, EventArgs e)
+    {
+        var userName = NameEntry.Text;
+        await Navigation.PushAsync(new GamePage(_gameMode, userName));
+    }
+}
