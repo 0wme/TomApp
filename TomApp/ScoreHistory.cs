8955264a223efde75c74821653bfba3a24056e3a
Tom <skixoxgaming@gmail.com>
2024-04-01 00:33:41 +0200

    Création de ScoreHistory.cs pour prendre les données du jeu, du nombre de réponse et du pseudo

---
+++ TomApp/ScoreHistory.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TomApp
+{
+    public static class ScoreHistory
+    {
+        private static List<Score> _scores = new List<Score>();
+
+        public static void AddScore(Score score)
+        {
+            _scores.Add(score);
+        }
+
+        public static IEnumerable<Score> GetScores(GameMode gameMode)
+        {
+            return _scores.Where(score => score.GameMode == gameMode).OrderByDescending(score => score.CorrectGuessCount);
+        }
+    }
+
+    public class Score
+    {
+        public string UserName { get; set; }
+        public GameMode GameMode { get; set; }
+        public int CorrectGuessCount { get; set; }
+    }
+}
