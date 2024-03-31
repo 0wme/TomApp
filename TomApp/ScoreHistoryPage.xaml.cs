8f2613635c50647d0e7720a099469e8d879d5ec6
Tom <skixoxgaming@gmail.com>
2024-04-01 00:33:55 +0200

    Création d'une page ScoreHistoryPage pour afficher les scores

---
+++ TomApp/ScoreHistoryPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TomApp;
+
+public partial class ScoreHistoryPage : ContentPage
+{
+    public ScoreHistoryPage()
+    {
+        InitializeComponent();
+    }
+
+    private void OnShowUnlimitedGameHistoryButtonClicked(object sender, EventArgs e)
+    {
+        ScoreListView.ItemsSource = ScoreHistory.GetScores(GameMode.Unlimited);
+    }
+
+    private void OnShowTenCountriesGameHistoryButtonClicked(object sender, EventArgs e)
+    {
+        ScoreListView.ItemsSource = ScoreHistory.GetScores(GameMode.TenCountries);
+    }
+
+    private void OnShowTenCountriesThreeChancesGameHistoryButtonClicked(object sender, EventArgs e)
+    {
+        ScoreListView.ItemsSource = ScoreHistory.GetScores(GameMode.TenCountriesThreeChances);
+    }
+}
