1084c1cd1683b70e94416f220aa53bd5e4dd5683
Tom <skixoxgaming@gmail.com>
2024-04-01 00:53:12 +0200

    Création de ScoreListPage pour afficher la liste des scores

---
+++ TomApp/ScoreListPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Maui.Controls;
+
+namespace TomApp
+{
+    public partial class ScoreListPage : ContentPage
+    {
+        public ScoreListPage(GameMode gameMode)
+        {
+            InitializeComponent();
+            ScoreListView.ItemsSource = ScoreHistory.GetScores(gameMode);
+        }
+    }
+}
