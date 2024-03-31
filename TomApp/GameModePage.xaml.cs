6455b9d9927c15f2b2abcc6cceb99a9ab7d592d5
Tom <skixoxgaming@gmail.com>
2024-04-01 00:07:56 +0200

    Création d'une page alternative pour proposer 3 mini jeux

---
+++ TomApp/GameModePage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TomApp
+{
+    public partial class GameModePage : ContentPage
+    {
+        public GameModePage()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnUnlimitedModeClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new GamePage(GameMode.Unlimited));
+        }
+
+        private async void OnTenCountriesModeClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new GamePage(GameMode.TenCountries));
+        }
+
+        private async void OnTenCountriesThreeChancesModeClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new GamePage(GameMode.TenCountriesThreeChances));
+        }
+    }
+    
+    public enum GameMode
+    {
+        Unlimited,
+        TenCountries,
+        TenCountriesThreeChances
+    }
+}
