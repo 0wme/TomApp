e48605b369c4fcd5a41bea2b8e3f8408a23e709e
Tom <skixoxgaming@gmail.com>
2024-04-07 19:02:19 +0200

    Implémentation de la page3

--- TomApp/Page3.xaml.cs
+++ TomApp/Page3.xaml.cs
@@ -1,9 +1,25 @@
-﻿namespace TomApp;
+﻿using Microsoft.Maui.Controls;
+using System;
 
-public partial class Page3 : ContentPage
+namespace TomApp
 {
-	public Page3()
-	{
-		InitializeComponent();
-	}
+    public partial class Page3 : ContentPage
+    {
+        public Page3()
+        {
+            InitializeComponent();
+        }
+
+        private void OnValidateButtonClicked(object sender, EventArgs e)
+        {
+            var country = new Country
+            {
+                Name = TitleEntry.Text,
+                Media = new Media { Flag = ImageEntry.Text },
+                Capital = DescriptionEntry.Text
+            };
+
+            MessagingCenter.Send(this, "AddCountry", country);
+        }
+    }
 }
