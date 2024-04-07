8f4d1cca30e1829220889e9db14674acae16e9f3
Tom <skixoxgaming@gmail.com>
2024-04-07 19:02:34 +0200

    Adaptation de la Page2 pour contenir les informations de la page3

--- TomApp/Page2ViewModel.xaml.cs
+++ TomApp/Page2ViewModel.xaml.cs
@@ -1,6 +1,11 @@
 using System.Collections.ObjectModel;
 using Microsoft.Maui.Dispatching;
 
+using System.Collections.ObjectModel;
+using Microsoft.Maui.Dispatching;
+using Microsoft.Maui.Controls;
+using TomApp;
+
 public class Page2ViewModel
 {
     private readonly ApiService _apiService;
@@ -11,6 +16,18 @@ public class Page2ViewModel
         _apiService = new ApiService();
         Countries = new ObservableCollection<Country>();
         LoadCountries();
+
+        MessagingCenter.Subscribe<Page3, Country>(this, "AddCountry", (sender, country) =>
+        {
+            var dispatcher = Dispatcher.GetForCurrentThread();
+            if (dispatcher != null)
+            {
+                dispatcher.Dispatch(() =>
+                {
+                    Countries.Add(country);
+                });
+            }
+        });
     }
 
     private async void LoadCountries()
